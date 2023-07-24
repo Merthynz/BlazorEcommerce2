70e90084744dfedb8532625e360bbdaaf43359b4
Merthynz <rerive@gmail.com>
2023-07-24 12:19:34 -0400

    Added ConnectionStrings on appsettings

--- BlazorEcommerce2/Server/Controllers/ProductController.cs
+++ BlazorEcommerce2/Server/Controllers/ProductController.cs
@@ -36,7 +36,7 @@ namespace BlazorEcommerce2.Server.Controllers
         };
 
         [HttpGet]
-        public async Task<IActionResult> GetProduct()
+        public async Task<ActionResult<List<Product>>> GetProduct()
         {
             return Ok(Products);
         }
